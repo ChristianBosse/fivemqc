aa884012a23731a492e2a997be75d90024bb51ca
christian <christianbosse1998@gmail.com>
2024-04-12 15:38:21 -0300

    frontend work

---
+++ frontend/src/components/serverExplorer/ServerExplorer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./ServerExplorer.css";
+
+const ServerExplorer = () => {
+    return (
+        // <div className="explorerContainer">
+        <div className="titleWrapper">
+            <p className="white-text">Rank</p>
+            <p className="white-text">Server Name</p>
+            <p className="white-text">Player Count</p>
+        </div>
+        // </div>
+    );
+};
+
+export default ServerExplorer;
--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./MainPage.css";
+import AddServer from "../components/addserver/AddServer";
+import ServerExplorer from "../components/serverExplorer/ServerExplorer";
 
 const MainPage = () => {
     return (
         <>
-            <div className="main-page">Hello</div>
+            <div className="main-page">
+                <div className="mainWrapper">
+                    <h1 className="addServer">
+                        Want to add your server? Contact us below!
+                    </h1>
+                    <AddServer />
+                </div>
+                <ServerExplorer />
+            </div>
         </>
     );
 };
